1fc3810f62642b51f499c76074a6eb9a0ab1cb69
Coder Nacho <inunrod820@g.educaand.es>
2025-12-21 19:04:48 +0100

    Ahora es más inteligente y usa las variables de entorno

--- scripts/run.ApachePHP-from-env.ps1
+++ scripts/run.ApachePHP-from-env.ps1
@@ -8,8 +8,6 @@ if (-not (Test-Path $envFile)) {
     Write-Error "Archivo de entorno '$envFile' no encontrado."
     exit 1
 }
-
-
 Get-Content $envFile | ForEach-Object {
     if ($_ -match '^\s*([^=]+)=(.*)$') {
         $envVars[$matches[1]] = $matches[2]
@@ -20,50 +18,72 @@ Get-Content $envFile | ForEach-Object {
 $imageName = $envVars['IMAGE_NAME']
 $containerName = $envVars['CONTAINER_NAME'] 
 $ip = $envVars['SERVER_IP']
+$serverport = $envVars['SERVER_PORT']
+$volumepath = $envVars['VOLUME_PATH']
+$foldername = $envVars['FOLDER_NAME']
+$datavolume = $envVars['DATA_VOLUME']
+$datafolder = $envVars['DATA_FOLDER']
 
-$moodleservername = $envVars['MOODLE_SERVER_NAME']
-$servername = $envVars['SERVER_NAME']
-$moodleserverport = $envVars['MOODLE_SERVER_PORT']
+$phpinfovolumepath = $envVars['PHPINFO_VOLUME_PATH']
+$phpinfofoldername = $envVars['PHPINFO_FOLDER_NAME']
 
-$MOODLE_VOLUME_PATH = $envVars['MOODLE_VOLUME_PATH']
-$volumePath = $envVars['VOLUME_PATH']
-$networkName = $envVars['NETWORK_NAME']
+$apachelogpath = $envVars['APACHE_LOG_PATH']
 
 if (
         $envVars['NETWORK_NAME'] -and `
         $envVars['NETWORK_SUBNET'] -and `
         $envVars['NETWORK_SUBNET_GATEWAY'] -and `
-        $envVars['IP'] -and `
+        $envVars['SERVER_IP'] -and `
         -not (docker network ls --filter "name=^${envVars['NETWORK_NAME]}$" --format "{{.Name}}")
     ) {
         $networkName = $envVars['NETWORK_NAME']
+        $networksubnet = $envVars['NETWORK_SUBNET']
+        $networksubnetgateway = $envVars['NETWORK_SUBNET_GATEWAY']
+        $networkDriver = $envVars['NETWORK_DRIVER']
         
         Write-Host "Creando red: $networkName"
-        docker network create $networkName --subnet=$($envVars['NETWORK_SUBNET']) --gateway=$($envVars['NETWORK_SUBNET_GATEWAY'])
+        docker network create $networkName --driver=$networkDriver --subnet=$networksubnet --gateway=$networksubnetgateway
+    }else{
+        Write-Warning "La red Docker ya existe o no se proporcionaron todos los parámetros necesarios."
     }
 
 
-# Eliminar contenedor si existe
 if (docker ps -a --filter "name=^${containerName}$" --format "{{.Names}}" | Select-Object -First 1) {
     Write-Host "Eliminando contenedor existente: $containerName"
     docker stop $containerName 2>$null
     docker rm $containerName 2>$null
 }
 
+# Limpiar contenido de la carpeta de logs de Apache si existe
+if (Test-Path $apachelogpath) {
+    Write-Host "Limpiando contenido de: $apachelogpath"
+    Remove-Item "$apachelogpath\*" -Force -Recurse
+}
+
+# Copiar archivo de configuración de 
+$ConfigSrc = ".\docker\http\moodle\config-dist.php"
+$ConfigDest = ".\moodle_src\config.php"
+
+if (Test-Path $ConfigSrc) {
+    Write-Host "Copiando configuración de : $ConfigSrc -> $ConfigDest"
+    Copy-Item -Path $ConfigSrc -Destination $ConfigDest -Force
+} else {
+    Write-Warning "Archivo de configuración no encontrado: $ConfigSrc"
+}
+
 # Ejecutar el contenedor Docker
 $dockerCmd = @(
     "docker run -d",
     "--name ${containerName}",
-    "-p ${moodleserverport}:80",
-    "-v ${volumePath}:/var/www/localhost/htdocs",
-    "-v ${MOODLE_VOLUME_PATH}:/var/www/${moodleservername}",
-    "-v .\logs\apachephp:/var/log/apache2",
+    "-p ${serverport}:80",
+    "-v ${phpinfovolumepath}:${phpinfofoldername}",
+    "-v ${volumepath}:${foldername}",
+    "-v ${datavolume}:${datafolder}",
+    "-v ${apachelogpath}:/var/log/apache2",
     "--env-file $envFile",
     "--hostname $containerName",
     "--network $networkName",
-    "--ip $ip",
-    "--add-host ${servername}:${ip}",
-    "--add-host ${moodleservername}:${ip}",
+    "--ip $ip"
     $imageName
 ) -join ' '
 
